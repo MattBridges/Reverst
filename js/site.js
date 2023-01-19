913b924a9d63278ea7e1d0c772bd591f1b2ab47e
MattBridges <Matthew.Bridges1@gmail.com>
2023-01-18 22:24:13 -0500

    Updated Footer Links

--- js/site.js
+++ js/site.js
@@ -1,10 +1,25 @@
-function DisplayMessage(){
-  let msg = document.getElementById('message').value;
-  Swal.fire(
-    {
-      backdrop:false,
-      title:'App Name',
-      text:msg
-    }
-  )
+//Entry Point
+function GetValues(){
+  //Get the input string
+  let inputString = document.getElementById('userString').value;
+
+  DisplayString(ReverseString(inputString));
+
+}
+
+//Logic Function
+function ReverseString(stringToReverse){
+  let result='';
+
+  for(let i=stringToReverse.length-1; i>=0;i--){
+    result += stringToReverse[i];
+  }  
+  return result;
+}
+
+//View Function
+function DisplayString(reversedString){
+  document.getElementById('results').textContent = reversedString;
+  document.getElementById('alert').classList.remove('invisible');
+
 }
